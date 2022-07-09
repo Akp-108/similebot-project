722ba753a0a5757968001a3438e79db1841e710e
Akp-108 <akp1081@gmail.com>
2022-07-09 13:37:49 +0530

    e-commerce

---
+++ src/Pages/Homes.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import {porducts} from '../componet/Json'
+import Card from '../componet/Card'
+import '../componet/css/Card.css'
+const Homes = () => {
+  return (
+    <div>
+      
+      <section className='cards__container'>{
+           porducts.details.map((v)=>{
+                return(
+                    <div>
+                <Card name = {v.name} 
+                    Img ={v.imageURL}
+                 price = {v.price} />
+                 </div>
+                )
+            })
+        }
+        </section>
+    </div>
+  )
+}
+
+export default Homes
